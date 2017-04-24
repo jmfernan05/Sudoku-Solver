eab19df401e38ff491c4d42ac885d6627564b511
jmfernan05 <jmfernandes5@gmail.com>
2017-04-24 17:55:40 -0500

    Add files via upload

---
+++ Sudoku.h
@@ -0,0 +1,37 @@
+//
+//  Sudoku.h
+//  Sudoku Solver
+//
+//  Created by Jenifer Fernandes on 4/21/17.
+//  Copyright © 2017 Jenifer Fernandes. All rights reserved.
+//
+
+#ifndef Sudoku_h
+#define UNASSIGNED 0
+#define N 9
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+class Sudoku
+{
+private:
+    bool checkRow (int grid[N][N], int row, int num); //Checks the row
+    bool checkCol (int grid[N][N], int col, int num); //Checks the column
+    bool checkBox (int grid[N][N], int startRow, int startCol, int num); //Checks each box
+    void PrintGrid(int grid [N][N]);
+    //void ReadFilstd::stringng filename0);
+    bool IsSafe(int grid [N][N], int&row, int&col, int num);
+    bool FindUnassignedLocation (int grid [N][N], int &row, int&col);
+    bool SolvePuzzle (int grid [N][N]);
+    
+public:
+    Sudoku ();
+    ~Sudoku() {}
+    
+};
+
+
+
+#endif /* Sudoku_h */
